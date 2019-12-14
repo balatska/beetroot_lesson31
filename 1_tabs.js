9b53a00e87b3effd11a0f88ca3fab33818d25924
balatska <t.i.balatska@gmail.com>
2019-12-14 22:39:55 +0200

    first commit

---
+++ 1_tabs.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(".tab-item").not(":first").hide();
+    $(".tab").click(function() {
+        $(".tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tab-item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+});
